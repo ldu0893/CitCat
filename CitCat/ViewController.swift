15453cdcf5b30b3efd9b5a5bbca282249cb19d5e
Larry <ldu0893@gmail.com>
2022-05-30 18:26:02 -0400

    snap to grid

--- CitCat/ViewController.swift
+++ CitCat/ViewController.swift
@@ -7,9 +7,49 @@
 
 import UIKit
 
+class GridView: UIView {
+    var gridWidth = 128
+    var gridHeight = 128
+    var lineWidth: CGFloat = 1.0
+    var lineColor: CGColor = UIColor.gray.cgColor
+    
+    override func draw(_ rect: CGRect) {
+        if let context = UIGraphicsGetCurrentContext() {
+
+            context.setLineWidth(lineWidth)
+            context.setStrokeColor(lineColor)
+
+            let numberOfColumns = Int(rect.width) / Int(gridWidth)
+            for i in 1...numberOfColumns {
+                var startPoint = CGPoint.zero
+                var endPoint = CGPoint.zero
+                startPoint.x = CGFloat(gridWidth * i)
+                startPoint.y = 0.0
+                endPoint.x = startPoint.x
+                endPoint.y = frame.size.height
+                context.move(to: CGPoint(x: startPoint.x, y: startPoint.y))
+                context.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+                context.strokePath()
+            }
+            let numberOfRows = Int(rect.height) / Int(gridHeight)
+            for j in 1...numberOfRows {
+                var startPoint = CGPoint.zero
+                var endPoint = CGPoint.zero
+                startPoint.x = 0.0
+                startPoint.y = CGFloat(gridHeight * j)
+                endPoint.x = frame.size.width
+                endPoint.y = startPoint.y
+                context.move(to: CGPoint(x: startPoint.x, y: startPoint.y))
+                context.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+                context.strokePath()
+            }
+        }
+    }
+}
+
 class ViewController: UIViewController {
     
-    
+    @IBOutlet var gridView: GridView!
     @IBOutlet var tempImageView: UIImageView!
     @IBOutlet var mainImageView: UIImageView!
     
@@ -19,14 +59,22 @@ class ViewController: UIViewController {
     var opacity: CGFloat = 1.0
     var straight: Bool = true
     
+    var gridWidth: CGFloat = 128.0
+    var gridHeight: CGFloat = 128.0 //are these supposed to be Floats or otherwise? idk
+    var grid: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gridView.draw(view.bounds)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return  }
         lastPoint = touch.location(in: view)
+        if grid {
+            lastPoint = CGPoint(x: round(lastPoint.x / gridWidth) * gridWidth, y: round(lastPoint.y / gridHeight) * gridHeight)
+        }
         
         
     }
@@ -34,14 +82,22 @@ class ViewController: UIViewController {
         guard let touch = touches.first else { return  }
         
         
-        let currentPoint = touch.location(in: view)
+        let currentPoint = touch.location(in: tempImageView)
         
-        if straight {
-       tempImageView.image = nil
-        drawLine(from: lastPoint, to: currentPoint)
+        var actualPoint = currentPoint
+        
+        if grid {
+            actualPoint = CGPoint(x: round(actualPoint.x / gridWidth) * gridWidth, y: round(actualPoint.y / gridHeight) * gridHeight)
+            if (lastPoint == actualPoint) { return  } //dunno if this works
+            print(actualPoint.y - lastPoint.y)
+            drawLine(from: lastPoint, to: actualPoint)
+            lastPoint = actualPoint
+        } else if straight {
+            tempImageView.image = nil
+            drawLine(from: lastPoint, to: actualPoint)
         } else {
-            drawLine(from: lastPoint, to: currentPoint)
-            lastPoint = currentPoint
+            drawLine(from: lastPoint, to: actualPoint)
+            lastPoint = actualPoint
         }
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -81,8 +137,13 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func gridMode(_ sender: Any) {
+        
+        grid = !grid
+        
+    }
     
-    
+       
     
     func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
         UIGraphicsBeginImageContext(view.frame.size)
